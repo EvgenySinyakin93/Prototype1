24888b2cab3474319f2504803ccbc6a571c2c40c
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-03-22 16:45:25 +0300

    Second job

--- src/js/Table.js
+++ src/js/Table.js
@@ -1,2 +1,66 @@
+export default function getDescription(characterObj) {
+  const result = [];
+  const { special } = characterObj;
+  if (!special) {
+    return result;
+  }
 
+  for (let i = 0; i < special.length; i++) {
+    if (!special[i].description) {
+      special.description = "Описание недоступно";
+    }
+    result.push({
+      id: special[i].id,
+      name: special[i].name,
+      icon: special[i].icon,
+      description: special[i].description,
+    });
+  }
+  return result;
+}
 
+const character = {
+  name: "Лучник",
+  type: "Bowman",
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+const hero1 = {
+    name: "Лучник",
+    type: "Bowman",
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: []
+};
+
+const hero2 = {
+    name: "Лучник",
+    type: "Bowman",
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+};
+
+console.log(character);
+console.log(hero1);
+console.log(hero2);
